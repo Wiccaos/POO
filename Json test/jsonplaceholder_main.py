b1e6955593c3c5fd80e8e842cf6ae547d0401530
Francisca Vidal <wiccaos.moon@outlook.com>
2024-12-03 16:33:28 -0300

    repost previous commit

--- Json test/jsonplaceholder_main.py
+++ Json test/jsonplaceholder_main.py
@@ -1,34 +1,4 @@
-import Servicios.ConsumirAPI
-
-def menu():
-    """Menú interactivo para utilizar las funciones"""
-    while True:
-        print(""" 
-Menú de JSONplaceholder
-1. Consultar Post
-2. Crear Post
-3. Salir
-""")
-        try: 
-            option = int(input("Ingrese una opción: "))
-        except ValueError:
-            print("Error: Debe ingresar un número")
-            continue  # Volver al inicio del bucle
-        
-        if option == 1:
-            post_id = int(input("Ingrese el ID del post que desea leer: "))
-            post = Servicios.ConsumirAPI.leer_post_por_id(post_id)
-            if post:
-                print(f"\nPost ID: {post.id}\nTítulo: {post.title}\nCuerpo: {post.body}")
-            
-        elif option == 2:    
-            created_post = Servicios.ConsumirAPI.crear_post()
-            if created_post:
-                print(f"\nPost creado exitosamente:\nPost ID: {created_post.id}\nTítulo: {created_post.title}\nCuerpo: {created_post.body}")
-        
-        elif option == 3:
-            print("Saliendo del programa...")
-            break
+from Negocio.menu import menu
 
 # Ejecutar menú
 menu()
